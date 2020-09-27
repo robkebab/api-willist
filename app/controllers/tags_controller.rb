05a8c2704f2d2d8f9291f44085d5930eb1dddee7
Tr1ckC0 <trick.co@yahoo.com>
2020-09-27 15:40:03 -0500

    RESTtags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,31 +1,49 @@
 class TagsController < ApplicationController
-    def index
+    before_action :find_tag, only: [:show, :update, :destroy]
 
+    def index
+        tags = Tag.all
+
+        render json: tags
     end
 
     def show
-
+        render json: @tag
     end
 
     def create
+        tag = Tag.new(tag_params)
 
+        if tag.save
+            render json: tag
+        else
+            render json: {"error": "the tag could not be created"}
+        end
     end
 
     def update
-
+        if @tag.update(tag_params)
+            render json: @tag
+        else
+            render json: {"error": "couldn't update that tag"}
+        end
     end
 
     def destroy
-
+        if @tag
+            @tag.destroy
+            render json: {"message": "tag deleted"}
+        else
+            render json: {"error": "could not find tag number #{params[:id]}"}
+        end
     end
 
     private
-
-        def find_course
-
+        def find_tag
+            @tag = Tag.find(params[:id])
         end
 
-        def course_params
-            
+        def tag_params
+            params.require(:tag).permit(:name)
         end
 end
