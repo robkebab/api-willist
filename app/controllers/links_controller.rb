8097ad082d9e78da12fb0462acd62322c0149eeb
Tr1ckC0 <trick.co@yahoo.com>
2020-09-27 15:29:39 -0500

    RESTlinks

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,31 +1,49 @@
 class LinksController < ApplicationController
-    def index
+    before_action :find_link, only: [:show, :update, :destroy]
 
+    def index
+        links = Link.all
+
+        render json: links
     end
 
     def show
-
+        render json: @link
     end
 
     def create
+        link = Link.new(link_params)
 
+        if link.save
+            render json: link
+        else
+            render json: {"error": "the link could not be created"}
+        end
     end
 
     def update
-
+        if @link.update(link_params)
+            render json: @link
+        else
+            render json: {"error": "couldn't update that link"}
+        end
     end
 
     def destroy
-
+        if @link
+            @link.destroy
+            render json: {"message": "link deleted"}
+        else
+            render json: {"error": "could not find link number #{params[:id]}"}
+        end
     end
 
     private
-
-        def find_course
-
+        def find_link
+            @link = Link.find(params[:id])
         end
 
-        def course_params
-            
+        def link_params
+            params.require(:link).permit(:text, :url, :course_id)
         end
 end
