276bfa03791719486b5761aa55a7f1c447fecf0f
Tr1ckC0 <trick.co@yahoo.com>
2020-09-27 13:53:24 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CourseTag.destroy_all
+LinkTag.destroy_all
+Tag.destroy_all
+Link.destroy_all
+Course.destroy_all
+User.destroy_all
+
+User.create(name: "Robert", password: "WTAwillist098123", curator: true)
+Course.create(title: "Javascript 30", blurb: "JS essentials in 30 days", user: User.first)
+Link.create(text: "Drum Kit", url: "https://www.youtube.com/watch?v=VuN8qwZoego&list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH", course: Course.first)
+Tag.create(name: "javascript")
+CourseTag.create(course: Course.first, tag: Tag.first)
+LinkTag.create(link: Link.first, tag: Tag.first)
